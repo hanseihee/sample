fd78db85ea6ffbffcc9470d0a2f332d1fa67af20
seiheehan <hanseihee@greenmonster.co>
2015-11-12 13:39:15 +0900

    push test

---
+++ sampleProject/sampleProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleProject
+//
+//  Created by seiheehan on 2015. 11. 12..
+//  Copyright © 2015년 greenmonster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sampleProject/sampleProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sampleProject
+//
+//  Created by seiheehan on 2015. 11. 12..
+//  Copyright © 2015년 greenmonster. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
